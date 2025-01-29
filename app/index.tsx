80b4f50d146d9e24c471f171115e67edd9f4444a
Austin Woolridge <batuhanok1375@gmail.com>
2025-01-29 10:26:31 +0100

    Start Disco button

--- app/index.tsx
+++ app/index.tsx
@@ -1,21 +1,65 @@
-import React from "react";
-import { StyleSheet, View } from 'react-native';
+import React, { useState } from "react";
+import { StyleSheet, View, TouchableHighlight, Button } from 'react-native';
+
 import HelloThere from "./components/HelloThere"
 
 export default function Index() {
 
+    const [colorBG, setColorBG] = useState('white');
+
+    let intervalId: any = null;
+
+    function changeColor() {
+        const r = Math.floor(Math.random() * 256);
+        const g = Math.floor(Math.random() * 256);
+        const b = Math.floor(Math.random() * 256);
+        setColorBG(`rgb(${r}, ${g}, ${b})`);
+
+    }
+
+    function startDisco() {
+        if (intervalId) {
+            clearInterval(intervalId);
+        }
+        intervalId = setInterval(changeColor, 250);
+        console.log("IndervalId set to: ", intervalId);
+
+    }
+
+    function stopDisco() {
+        clearInterval(intervalId);
+        intervalId = null;
+        console.log("IndervalId set to: ", intervalId);
+    }
+
+
+
 
     return (
-        <View style={{ height: 500, width: 500 }}>
-            <HelloThere />
-            <HelloThere />
-            <HelloThere />
-            <HelloThere />
-            <HelloThere />
-            <HelloThere />
-            <HelloThere />
-            <HelloThere />
-        </View>
+        <TouchableHighlight style={{ backgroundColor: colorBG }}>
+            <View>
+                <View style={{ height: 500, width: 500 }}>
+                    <HelloThere />
+                    <HelloThere />
+                    <HelloThere />
+                    <HelloThere />
+                    <HelloThere />
+                    <HelloThere />
+                    <HelloThere />
+                    <HelloThere />
+                </View>
+                <Button
+                    title="Disco"
+                    onPress={startDisco}
+                />
+                <Button
+                    title="Stop Disco"
+                    onPress={stopDisco}
+                />
+            </View>
+
+        </TouchableHighlight>
+
 
     );
 }
@@ -26,6 +70,9 @@ const styles = StyleSheet.create(
             justifyContent: 'center',
             alignItems: 'center',
             flex: 1,
-        }
+        },
+        background: {
+            backgroundColor: 'red'
+        },
     }
 );
